b83a4ae9e25a79e77434ef46ab64db01f72c24fa
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2025-11-03 13:53:18 +0300

    Clean tree

--- vaxtrack_backend/vaxtrack/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/", include("users.urls")),
-    path("api/v1/", include("registry.urls")),
-    path("api/v1/", include("analytics.urls")),
-]
